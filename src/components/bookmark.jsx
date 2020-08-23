dc6b16ca7632185ff6f4fd0137355ddd5769cd7c
geraldgsh <geraldgsh@hotmail.com>
2020-08-24 00:24:24 +0800

    built card layout

---
+++ src/components/bookmark.jsx
@@ -0,0 +1,132 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { config, library } from '@fortawesome/fontawesome-svg-core';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import '../styles/components.scss';
+
+config.autoAddCss = false;
+library.add(fab);
+
+const projects = [
+  {
+    id: 1,
+    img: '/img/grid.jpg',
+    desc: 'Built a custom grid-based framework (similar to bootstrap) with basic functionality necessary to build a website.',
+    stack: ' (HTML + CSS)',
+    live: 'https://rawcdn.githack.com/davitomix/CustomGridFrameWork/b5320d1646cea5bc2e0e9bdbcfe881e37083e70f/index.html',
+    github: 'https://github.com/davitomix/CustomGridFrameWork',
+  },
+  {
+    id: 2,
+    img: '/img/entracker.jpg',
+    desc: 'Built a form based React/Redux site with Rails Backend API to Create and Read data.',
+    stack: ' (React + Rails + API + Heroku)',
+    live: 'https://energy-track.herokuapp.com/',
+    github: 'https://github.com/geraldgsh/energy-tracker',
+  },
+  {
+    id: 3,
+    img: '/img/social.jpg',
+    desc: 'A Facebook-like social network application. Features – users, profiles, “friending”, posts, newsfeed, and likes.',
+    stack: ' (Ruby on Rails + Heroku)',
+    live: 'https://societalbook.herokuapp.com/',
+    github: 'https://github.com/geraldgsh/societalbook',
+  },
+  {
+    id: 4,
+    img: '/img/todo.jpg',
+    desc: 'An Objected Oriented Javascript To Do List app that uses local storage on browser.',
+    stack: ' (HTML + CSS + JS + Bulma)',
+    live: 'https://rawcdn.githack.com/geraldgsh/todo-list/10b5955ab59a3ca8048f348bf3ed253ffd558b90/dist/index.html',
+    github: 'https://github.com/geraldgsh/todo-list',
+  },
+  {
+    id: 5,
+    img: '/img/weather.jpg',
+    desc: 'A weather application that shows weather info on queried location(s) via API from openweathermap.',
+    stack: ' (HTML + CSS + JS + API)',
+    live: 'https://raw.githack.com/geraldgsh/weather-app/master/dist/index.html',
+    github: 'https://github.com/geraldgsh/weather-app',
+  },
+  {
+    id: 6,
+    img: '/img/calculator.jpg',
+    desc: 'A simple calculator app built with React. It uses Stateful component which are defined using a class.',
+    stack: ' (React + Heroku)',
+    live: 'https://react-calculate.herokuapp.com/',
+    github: 'https://github.com/geraldgsh/react-calculator',
+  },
+  {
+    id: 7,
+    img: '/img/calculator.jpg',
+    desc: 'A simple calculator app built with React. It uses Stateful component which are defined using a class.',
+    stack: ' (React + Heroku)',
+    live: 'https://react-calculate.herokuapp.com/',
+    github: 'https://github.com/geraldgsh/react-calculator',
+  },
+];
+
+function SearchBar() {
+  return (
+    <div className="column social-media">
+      <form className="field has-addons">
+        <input
+          className="input"
+          type="text"
+          placeholder="GitHub username"
+          required
+        />
+        <button className="button is-info" type="button">
+          Add card
+        </button>
+      </form>
+    </div>
+  );
+}
+
+function Bookmark() {
+  return (
+    <div id="list" className="card-container">
+      {projects.map(project => (
+        <div className="card" key={project.id}>
+          <div className="card-image">
+            <div className="avatar-header">
+              <div className="container has-text-centered">
+                <div className="column pale">
+                  <br />
+                  <div className="boxster">
+                    <figure className="avatar">
+                      <img src="https://randomuser.me/api/portraits/women/67.jpg" alt="GH logo" />
+                    </figure>
+                    <form>
+                      <div className="user-meta has-text-centered">
+                        <a href={project.github} target={project.live}>
+                          <i>
+                            <FontAwesomeIcon icon={['fab', 'github-square']} width="18" />
+                          </i>
+                        </a>
+                        <h3 className="username">Helen Miller</h3>
+                        <h5 className="position">Bookmark</h5>
+                      </div>
+                    </form>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="card-content">
+            <div className="desc is-capitalized">
+              {project.desc}
+              {project.stack}
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export {
+  Bookmark,
+  SearchBar,
+};
--- src/containers/main.jsx
+++ src/containers/main.jsx
@@ -1,6 +1,11 @@
+/* eslint-disable react/destructuring-assignment */
 import React, { Component } from 'react';
 import { auth } from '../services/firebase';
 import '../styles/main.scss';
+
+import { Bookmark, SearchBar } from '../components/bookmark';
+import Followers from '../components/followers';
+import Following from '../components/following';
 
 class Main extends Component {
   constructor(props) {
@@ -11,21 +16,99 @@ class Main extends Component {
     this.handleClick = this.handleClick.bind(this);
   }
 
+  handleClick(e) {
+    const sector = e.target.value;
+    if (sector === 'Following') {
+      this.setState(() => ({
+        section: <Following />,
+      }));
+    } else if (sector === 'Followers') {
+      this.setState(() => ({
+        section: <Followers />,
+      }));
+    } else {
+      this.setState(() => ({
+        section: <Bookmark />,
+      }));
+    }
+  }
+
   render() {
+    const sector = this.state.section;
+    let search;
+    if (sector.type.name === 'Bookmark') {
+      search = <SearchBar />;
+    } else {
+      search = null;
+    }
     return (
-      <div className="home">
-        <section>
-          <div className="jumbotron jumbotron-fluid py-5">
-            <div className="container text-center py-5">
-              <h1 className="display-4">Welcome to Chatty</h1>
-              <p className="lead">A great place to share your thoughts with friends</p>
-              <button className="btn btn-primary mr-3" onClick={() => auth().signOut()}>Logout</button>
+      <section className="hero is-info is-fullheight">
+        <div className="hero-body">
+          <div className="container is-fluid">
+            <div className="columns is-centered is-mobile">
+              <div className="boxer">
+                <div className="columns is-gapless">
+                  <div className="sidebar column is-3">
+                    <div className="rows columns is-centered is-gapless">
+                      <div className="row">
+                        <div className="column">
+                          <figure className="image is-128x128 pic">
+                            <img className="is-rounded" src="https://avatars3.githubusercontent.com/u/29500109?v=4" alt="profile pic" />
+                          </figure>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="rows columns is-centered is-gapless">
+                      <div className="row">
+                        <div className="wording column has-text-centered">
+                          <h1 className="title has-text-black">Gerald Goh</h1>
+                          <h2 className="subtitle has-text-black">Full Stack Developer</h2>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="rows columns is-centered is-gapless">
+                      <div className="container is-fluid">
+                        <div className="column">
+                          <div className="row">
+                            <div className="column buttons" value="example" role="presentation" onClick={e => this.handleClick(e, 'value')}>
+                              <button value="Bookmark" name="Bookmark" id="#bookmark" className="button is-primary is-fullwidth" type="button">
+                                Bookmark
+                              </button>
+                              <button value="Followers" name="Followers" id="#followers" className="button is-link is-fullwidth" type="button">
+                                Followers
+                              </button>
+                              <button value="Following" name="Following" id="#following" className="button is-success is-fullwidth" type="button">
+                                Following
+                              </button>
+                            </div>
+                            <div className="column buttons">
+                              <button className="button is-danger is-fullwidth" type="button" onClick={() => auth().signOut()}>Logout</button>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="rows columns is-centered is-gapless">
+                      <div className="container is-fluid">
+                        <div className="column">
+                          <div className="row">
+                            {search}
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="column is-9">
+                    {sector}
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
-        </section>
-      </div>
+        </div>
+      </section>
     );
-  }  
+  }
 }
 
 export default Main;
